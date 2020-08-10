a33db2ad4d15fd0b68de3885ac1cebe6caede005
Sviatoslav Maha <svatoslavmaga@gmail.com>
2020-08-10 17:58:58 +0300

    Updates

---
+++ precache-manifest.b86478588356ff2b9047afc7a53f7e69.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8eb14cb38a7de90f6d71034bb9a1f12d",
+    "url": "/github-pages-create-react-app/index.html"
+  },
+  {
+    "revision": "e8bcf12641c204ae67b0",
+    "url": "/github-pages-create-react-app/static/css/2.c18faaca.chunk.css"
+  },
+  {
+    "revision": "de295681f5feb0a68784",
+    "url": "/github-pages-create-react-app/static/css/main.083d573e.chunk.css"
+  },
+  {
+    "revision": "e8bcf12641c204ae67b0",
+    "url": "/github-pages-create-react-app/static/js/2.53b6d761.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/github-pages-create-react-app/static/js/2.53b6d761.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de295681f5feb0a68784",
+    "url": "/github-pages-create-react-app/static/js/main.ac9e9f81.chunk.js"
+  },
+  {
+    "revision": "7051f257415e1003bef3",
+    "url": "/github-pages-create-react-app/static/js/runtime-main.2fb472c7.js"
+  }
+]);
